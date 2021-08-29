73f3f8ab49b710f795560268bb8f3c9e07034b2b
Reese Gick <reesegick@yahoo.com>
2021-08-29 09:30:16 -0700

    master

--- config/settings.py
+++ config/settings.py
@@ -129,3 +129,14 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'accounts.CustomUser'
+
+
+
+
+
+
+
+
+
+
+EMAIL_BACKEND='django.core.mail.backends.console.EmailBackend'
